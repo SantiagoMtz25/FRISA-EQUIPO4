111b247194da4ea8bae391e7b5f464528a56a730
SantiagoMtz25 <santimtzv01@gmail.com>
2023-09-13 10:00:57 -0600

    Dropdown menu item working, nagivation between screens confirmed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,8 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.7.1")
+    // Added to try and make work the DropdownMenuItem
+    implementation("androidx.compose.runtime:runtime:1.0.1")
+    implementation("androidx.compose.foundation:foundation:1.0.1")
+    implementation("androidx.compose.material:material:1.0.1")
 }
--- app/src/main/java/com/example/loginpagetest/screens/homepage/HomePage.kt
+++ app/src/main/java/com/example/loginpagetest/screens/homepage/HomePage.kt
@@ -1,46 +1,49 @@
 package com.example.loginpagetest.screens.homepage
-
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.compose.material3.Text
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.navigation.NavHostController
+
 
 @Composable
 fun OrganizationsCatalogue(content: NavHostController) {
     val query = remember { mutableStateOf(TextFieldValue()) }
-
-    // Search Bar
     BasicTextField(
         value = query.value,
         onValueChange = { query.value = it },
         modifier = Modifier.fillMaxSize()
     )
 
-    // Catalogue List
     LazyColumn {
         items(DataProvider.categories) { category ->
-            if (category.name.contains(query.value.text, true) ||
-                category.organizations.any { it.name.contains(query.value.text, true) }) {
-                CategoryItem(category = category)
-            }
+            CategoryItem(category = category)
         }
     }
 }
 
 @Composable
 fun CategoryItem(category: Category) {
-    Column {
-        Text(text = category.name)
-        for (org in category.organizations) {
-            Text(text = " - ${org.name}")
+    var expanded by remember { mutableStateOf(false) }
+    Text(text = category.name, modifier = Modifier.clickable { expanded = !expanded })
+    DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+        category.organizations.forEach { organization ->
+            DropdownMenuItem(onClick = { expanded = false }) {
+                Text(text = organization.name)
+            }
         }
     }
 }
