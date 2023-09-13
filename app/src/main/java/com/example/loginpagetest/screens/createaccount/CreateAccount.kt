4ae74c5ef5bf08d82496e4d8ec7392f86ef4a858
SantiagoMtz25 <santimtzv01@gmail.com>
2023-09-12 19:14:43 -0600

    Create account options

--- app/src/main/java/com/example/loginpagetest/screens/createaccount/CreateAccount.kt
+++ app/src/main/java/com/example/loginpagetest/screens/createaccount/CreateAccount.kt
@@ -1,9 +1,81 @@
 package com.example.loginpagetest.screens.createaccount
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.loginpagetest.R
 
 @Composable
-fun CreateAccount(content: NavHostController) {
+fun CreateAccount(navController: NavHostController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            var name by rememberSaveable { mutableStateOf("") }
+            var lastName by rememberSaveable { mutableStateOf("") }
+            var email by rememberSaveable { mutableStateOf("") }
+            var password by rememberSaveable { mutableStateOf("") }
+            var confirmPassword by rememberSaveable { mutableStateOf("") }
+            var phoneNumber by rememberSaveable { mutableStateOf("") }
+            var state by rememberSaveable { mutableStateOf("") }
+            var city by rememberSaveable { mutableStateOf("") }
+            val image: Painter = painterResource(id = R.drawable.frisa)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp, 100.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
 
+            CreateAccountTextField(value = name, onValueChange = { name = it }, label = "Name")
+            CreateAccountTextField(value = lastName, onValueChange = { lastName = it }, label = "Last Name")
+            CreateAccountTextField(value = email, onValueChange = { email = it }, label = "Email", keyboardType = KeyboardType.Email)
+            CreateAccountTextField(value = password, onValueChange = { password = it }, label = "Password", keyboardType = KeyboardType.Password)
+            CreateAccountTextField(value = confirmPassword, onValueChange = { confirmPassword = it }, label = "Confirm Password", keyboardType = KeyboardType.Password)
+            CreateAccountTextField(value = phoneNumber, onValueChange = { phoneNumber = it }, label = "Phone Number", keyboardType = KeyboardType.Phone)
+            CreateAccountTextField(value = state, onValueChange = { state = it }, label = "State")
+            CreateAccountTextField(value = city, onValueChange = { city = it }, label = "City/Municipality")
+            Button(onClick = {
+                // println("Create Account button clicked")
+                navController.navigate("login")
+            }) {
+                Text("Create Account")
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateAccountTextField(value: String, onValueChange: (String) -> Unit, label: String, keyboardType: KeyboardType = KeyboardType.Text) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = ImeAction.Done
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, bottom = 8.dp)
+    )
 }
--- app/src/main/java/com/example/loginpagetest/screens/loginpage/LoginPage.kt
+++ app/src/main/java/com/example/loginpagetest/screens/loginpage/LoginPage.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -56,8 +57,8 @@ fun mainLoginPage(navController: NavHostController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        var email by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
+        var email by rememberSaveable { mutableStateOf("") }
+        var password by rememberSaveable { mutableStateOf("") }
 
         // FRISA Logo
         val image: Painter = painterResource(id = R.drawable.frisa)
